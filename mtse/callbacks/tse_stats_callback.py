94cdd1fa4faada9a11a492fd9fd33fa763d6e4f7
Ethan L. Mines <elmines@users.noreply.github.com>
2025-07-29 16:15:32 -0400

    Add more metrics

--- mtse/callbacks/tse_stats_callback.py
+++ mtse/callbacks/tse_stats_callback.py
@@ -19,6 +19,18 @@ class TSEStatsCallback(Callback):
         self.__pred_pos = 0
         self.__support = 0
 
+        self.__wrongtarg = 0
+        self.__wrongstance = 0
+
+    def reset(self):
+        self.__summarized = False
+        self.__tp = 0
+        self.__pred_pos = 0
+        self.__support = 0
+
+        self.__wrongtarg = 0
+        self.__wrongstance = 0
+
 
     @staticmethod
     def compute_metrics(tp, pred_pos, support) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -40,24 +52,22 @@ class TSEStatsCallback(Callback):
         if self.__summarized:
             raise ValueError("Must reset F1Calc before recording more results")
 
-        batch_pred_pos = int(torch.sum(target_preds != self.no_target))
-        self.__pred_pos += batch_pred_pos
-        batch_support = int(torch.sum(target_labels != self.no_target))
-        self.__support += batch_support
+        pred_pos = target_preds != self.no_target
+        count_pred_pos = int(torch.sum(pred_pos))
+        count_support = int(torch.sum(target_labels != self.no_target))
+        self.__pred_pos += count_pred_pos
+        self.__support += count_support
 
-        label_has_target = torch.where(target_labels != self.no_target)
-        target_preds = target_preds[label_has_target]
-        stance_preds = stance_preds[label_has_target]
-        target_labels = target_labels[label_has_target]
-        stance_labels = stance_labels[label_has_target]
+        label_has_target = target_labels != self.no_target
+        label_has_target_inds = torch.where(label_has_target)
+        target_preds = target_preds[label_has_target_inds]
+        stance_preds = stance_preds[label_has_target_inds]
+        target_labels = target_labels[label_has_target_inds]
+        stance_labels = stance_labels[label_has_target_inds]
 
+        self.__wrongtarg   += int(torch.sum(target_preds != target_labels))
+        self.__wrongstance += int(torch.sum(torch.logical_and(target_preds == target_labels, stance_preds != stance_labels)))
         self.__tp += int(torch.sum(torch.logical_and(target_preds == target_labels, stance_preds == stance_labels)))
-
-    def reset(self):
-        self.__summarized = False
-        self.__tp = 0
-        self.__pred_pos = 0
-        self.__support = 0
 
     def on_validation_epoch_start(self, trainer, pl_module):
         self.reset()
@@ -77,6 +87,14 @@ class TSEStatsCallback(Callback):
         return self._on_epoch_end(trainer, pl_module, "test")
     def _on_epoch_end(self, trainer, pl_module: L.LightningModule, stage):
         results = {}
-        _, _2, results['tse_f1'] = TSEStatsCallback.compute_metrics(self.__tp, self.__pred_pos, self.__support)
-        for (k, v) in filter(lambda pair: pair[0].endswith('f1'), results.items()):
+        _, _2, results['tse_f1'] = \
+            TSEStatsCallback.compute_metrics(self.__tp, self.__pred_pos, self.__support)
+        results['wrongtarg'] = self.__wrongtarg
+        results['wrongstance'] = self.__wrongstance
+        results['tp'] = self.__tp
+        results['pred_pos'] = self.__pred_pos
+        results['support'] = self.__support
+
+        results = {f"{stage}_{k}":v for k,v in results.items()}
+        for (k, v) in results.items():
             pl_module.log(k, v, on_step=False, on_epoch=True)
