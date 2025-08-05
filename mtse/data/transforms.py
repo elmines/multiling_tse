52c66304fe1d15a63a11ff860b90fadc7a0f1221
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-05 14:41:20 -0400

    Clean up transform code

--- mtse/data/transforms.py
+++ mtse/data/transforms.py
@@ -26,28 +26,58 @@ class SemHashtagRemoval(Transform):
 
 class LiKeywordRemoval(Transform):
 
-    def __init__(self):
-        # Replacement dict is from Li et al. (2023)'s work
-        replace_strings = {
-            'PStance'      : ['Joe', 'Biden', 'Bernie', 'Sanders', 'Donald', 'Trump'],
-            'AM'           : ['abortion', 'cloning', 'death', 'penalty', 'gun', 'control', 'marijuana', 'legalization', 'minimum', 'wage', 'nuclear', 'energy', 'school', 'uniforms'],
-            'SemEval2016'  : ['Atheism', 'Feminist', 'Movement', 'Hillary',  'Clinton', 'Legalization', 'Abortion'],
-            'Covid19'      : ['face', 'masks', 'fauci', 'stay', 'home', 'orders', 'school', 'closures'],
-            'Stance_Merge_Unrelated': ['Joe', 'Biden', 'Bernie', 'Sanders', 'Donald', 'Trump', 'abortion', 'cloning', 'death', 'penalty', 'gun', 'control', 'marijuana', 'legalization', 'minimum', 'wage',  'nuclear', 'energy', 'school', 'uniforms', 'Atheism', 'Feminist', 'Movement', 'Hillary', 'Clinton', 'face', 'masks', 'fauci', 'stay', 'home', 'school', 'closures', 'orders']
-        }
-        merged_keywords = {k.lower() for keyword_set in replace_strings.values() for k in keyword_set}
-        self.pattern = re.compile('|'.join(merged_keywords), flags=re.IGNORECASE)
+    # Replacement dict is from Li et al. (2023)'s work
+    KEYWORDS =  [
+        'Joe',
+        'Biden',
+        'Bernie',
+        'Sanders',
+        'Donald',
+        'Trump',
+        'abortion',
+        'cloning',
+        'death',
+        'penalty',
+        'gun',
+        'control',
+        'marijuana',
+        'legalization',
+        'minimum',
+        'wage',
+        'nuclear',
+        'energy',
+        'school',
+        'uniforms',
+        'Atheism',
+        'Feminist',
+        'Movement',
+        'Hillary',
+        'Clinton',
+        'face',
+        'masks',
+        'fauci',
+        'stay',
+        'home',
+        'school',
+        'closures',
+        'orders'
+    ]
+
+    KEYWORD_PATT = re.compile('|'.join(KEYWORDS), flags=re.IGNORECASE)
 
     def __call__(self, sample: Sample):
-        sample.context = self.pattern.sub('', sample.context)
+        old_context = sample.context
+        new_context, count = LiKeywordRemoval.KEYWORD_PATT.subn('', old_context)
+        sample.context = new_context
 
 class LiPreprocess(Transform):
 
     KEYWORD_DICT: Optional[Dict[str, str]] = None
 
+    PHRASE_PATTERN = re.compile(r"[A-Za-z#@]+|[,.!?&/\<>=$]|[0-9]+")
+
     def __init__(self):
         self._keyword_dict = LiPreprocess.get_keyword_dict()
-        self.phrase_pattern = re.compile(r"[A-Za-z#@]+|[,.!?&/\<>=$]|[0-9]+")
 
     @classmethod
     def get_keyword_dict(cls) -> Dict[str, str]:
@@ -71,7 +101,7 @@ class LiPreprocess(Transform):
         twp_cleaned = twp.clean(old_context)
 
         converted = []
-        phrases = self.phrase_pattern.findall(twp_cleaned)
+        phrases = LiPreprocess.PHRASE_PATTERN.findall(twp_cleaned)
         for phrase in phrases:
             lowered = phrase.lower()
             if lowered in self._keyword_dict:
