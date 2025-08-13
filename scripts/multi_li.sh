265075e4635cc20832a5a5efcb52aaac3c4899a0
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-13 12:53:36 -0400

    Update multi_li script

--- scripts/multi_li.sh
+++ scripts/multi_li.sh
@@ -1,43 +1,104 @@
 #!/bin/bash
 
-for seed in 0 112 343
-do
-    target_exp_name=LiTargetClassifier_seed${seed}
+TARGET_FIT=${TARGET_FIT:-1}
+TARGET_TEST=${TARGET_TEST:-1}
+TARGET_PRED=${TARGET_PRED:-1}
+STANCE_FIT=${STANCE_FIT:-1}
 
-    python -m mtse fit \
-        -c configs/full/li_target_classifier.yaml \
-        --trainer.logger.save_dir ./lightning_logs \
-        --trainer.logger.name MultiLi \
-        --trainer.logger.version $target_exp_name \
-        --seed_everything $seed
+SAVE_DIR=./lightning_logs
+EXP_NAME=MultiLi
+LOGGER_ARGS="--trainer.logger.save_dir $SAVE_DIR --trainer.logger.name $EXP_NAME"
 
 
-    train_dir=lightning_logs/MultiLi/$target_exp_name
-    ckpt_path=$(ls $train_dir/checkpoints/*ckpt)
+function get_target_train_version
+{
+    seed=$1
+    echo LiTargetClassifier_seed${seed}
+}
+function get_target_predict_version
+{
+    seed=$1
+    echo $(get_target_train_version $seed)_predict
+}
 
-    python -m mtse test \
-        -c $train_dir/config.yaml \
-        --data configs/data/li_tse_target_test_split.yaml \
-        --trainer.logger.version ${target_exp_name}_test \
-        --ckpt_path $ckpt_path
+function get_target_train_dir
+{
+    seed=$1
+    echo $SAVE_DIR/$EXP_NAME/$(get_target_train_version $seed)
+}
+function get_target_preds_dir
+{
+    seed=$1
+    echo $SAVE_DIR/$EXP_NAME/$(get_target_predict_version $seed)
+}
 
-    python -m mtse predict \
-        -c $train_dir/config.yaml \
-        --data configs/data/li_tse_target_predict_all.yaml \
-        --trainer.logger.version ${target_exp_name}_predict \
-        --ckpt_path $ckpt_path
+function get_stance_exp_name
+{
+    seed=$1
+    echo LiStanceClassifier_seed${seed}
+}
 
-    continue
+function get_stance_train_dir
+{
+    seed=$1
+    echo $SAVE_DIR/$EXP_NAME/$(get_stance_exp_name $seed)
+}
 
-    ########## Stage 2: Stance training and TSE ##############
+if [ $TARGET_FIT -eq 1 ]
+then
+    for seed in 0 112 343
+    do
+        python -m mtse fit \
+            -c configs/full/li_target_classifier.yaml \
+            $LOGGER_ARGS \
+            --trainer.logger.version $(get_target_train_version $seed) \
+            --seed_everything $seed
+    done
+else
+    echo "Skipping target fitting"
+fi
 
-    tse_exp_name=LiTse_seed${seed}
-    python -m mtse predict \
-        -c configs/full/li_target_classifier.yaml \
-        --trainer.logger.save_dir ./lightning_logs \
-        --trainer.logger.name MultiLi \
-        --trainer.logger.version $tse_exp_name \
-        --seed_everything $seed
+if [ $TARGET_TEST -eq 1 ]
+then
+    for seed in 0 112 343
+    do
+        python -m mtse test \
+            -c $(get_target_train_dir $seed)/config.yaml \
+            --data configs/data/li_tse_target_test_split.yaml \
+            --trainer.logger.version $(get_target_train_version $seed)_test \
+            --ckpt_path $(ls $(get_target_train_dir $seed)/checkpoints/*ckpt)
+    done
+else
+    echo "Skipping target testing"
+fi
 
+if [ $TARGET_PRED -eq 1 ]
+then
+    for seed in 0 112 343
+    do
+        python -m mtse predict \
+            -c $(get_target_train_dir $seed)/config.yaml \
+            --data configs/data/li_tse_target_predict_all.yaml \
+            --trainer.logger.version $(get_target_predict_version $seed) \
+            --ckpt_path $(ls $(get_target_train_dir $seed)/checkpoints/*ckpt)
+    done
+else
+    echo "Skipping target prediction"
+fi
 
-done
+if [ $STANCE_FIT -eq 1 ]
+then
+    for seed in 0 112 343
+    do
+        python -m mtse fit \
+            -c configs/full/li_stance_classifier.yaml \
+            $LOGGER_ARGS \
+            --data.stance_train_corpus.target_preds_path $(get_target_preds_dir $seed)/target_preds.0.txt \
+            --data.target_train_corpus.target_preds_path $(get_target_preds_dir $seed)/target_preds.0.txt \
+            --data.val_corpus.target_preds_path $(get_target_preds_dir $seed)/target_preds.1.txt \
+            --trainer.logger.version $(get_stance_exp_name $seed) \
+            --seed_everything $seed
+    done
+else
+    echo "Skipping stance fitting"
+fi
