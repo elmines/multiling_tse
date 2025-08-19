58f17f42afc6d073592ac765ae8fec5fc8a95953
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-19 15:02:24 -0400

    Update split_tse

--- utils/split_tse.py
+++ utils/split_tse.py
@@ -9,37 +9,29 @@ import csv
 
 ENCODING = 'ISO-8859-1'
 
-def copy_se_rows(source_path, out_path):
+def part_corpus(source_path, corpus_slices):
     with open(source_path, 'r', encoding=ENCODING) as r:
         reader = csv.DictReader(r)
         fieldnames = reader.fieldnames
-        train_rows = list(reader) 
-    se_trainrows = [row for row in train_rows if '#SemST' in row['Tweet']]
-    with open(out_path, 'w', encoding=ENCODING) as w:
-        writer = csv.DictWriter(w, fieldnames=fieldnames, lineterminator='\n')
-        writer.writeheader()
-        writer.writerows(se_trainrows)
-
-if __name__ == "__main__":
-
-    data_dir = os.path.join("data", "li_tse")
-
-    with open(os.path.join(data_dir, "raw_test_all_onecol.csv"), 'r', encoding=ENCODING) as r:
-        reader = csv.DictReader(r)
-        fieldnames = reader.fieldnames
-        test_rows = list(reader)
-    corpus_rows = {
-        "test_semeval.csv": test_rows[:1080],
-        "test_covid.csv": test_rows[1080:1880],
-        "test_am.csv": test_rows[1880:6989],
-        "test_pstance.csv": test_rows[6989:9146],
-        "test_unrelated.csv": test_rows[9146:]
-    }
-    for (filename, rowset) in corpus_rows.items():
-        with open(os.path.join(data_dir, filename), 'w', encoding=ENCODING) as w:
+        all_rows = list(reader)
+    for filename, corpus_slice in corpus_slices.items():
+        rowset = all_rows[corpus_slice]
+        with open(filename, 'w', encoding=ENCODING) as w:
             writer = csv.DictWriter(w, fieldnames=fieldnames, lineterminator='\n')
             writer.writeheader()
             writer.writerows(rowset)
 
-    copy_se_rows(os.path.join(data_dir, 'raw_train_all_onecol.csv'), os.path.join(data_dir, 'train_semeval.csv'))
-    copy_se_rows(os.path.join(data_dir, 'raw_val_all_onecol.csv'), os.path.join(data_dir, 'val_semeval.csv'))
+
+if __name__ == "__main__":
+
+    data_dir = os.path.join("data", "li_tse")
+    add_dir_prefix = lambda slices: {os.path.join(data_dir, k):v for k,v in slices.items()}
+
+    test_slices = {
+        "test_semeval.csv": slice(None, 1080),
+        "test_covid.csv": slice(1080,1880),
+        "test_am.csv": slice(1880,6989),
+        "test_pstance.csv": slice(6989,9146),
+        "test_unrelated.csv": slice(9146, None)
+    }
+    part_corpus(os.path.join(data_dir, 'raw_test_all_onecol.csv'), add_dir_prefix(test_slices))
