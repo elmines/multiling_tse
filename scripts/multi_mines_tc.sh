27f15bb18834fd0d58395e30fc579300d99d5e3e
Ethan L. Mines <elmines@users.noreply.github.com>
2025-08-29 10:53:21 -0400

    Update multi_mines_tc.sh

--- scripts/multi_mines_tc.sh
+++ scripts/multi_mines_tc.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 ALL=${ALL:-0}
-FIT=${FIT:-$ALL}
 
+FIT=${FIT:-$ALL}
 TARGET_TEST=${TARGET_TEST:-$ALL}
 STANCE_TEST=${STANCE_TEST:-$ALL}
 TSE_TEST=${TSE_TEST:-$ALL}
-GT_TSE_TEST=${GT_TSE_TEST:-$ALL}
 
 SEEDS=${@:- 0 112 343}
 
 SAVE_DIR=${SAVE_DIR:-./lightning_logs}
-EXP_NAME=${EXP_NAME:-MultiLiTc}
+EXP_NAME=${EXP_NAME:-MultiMinesTc}
 LOGS_ROOT=$SAVE_DIR/$EXP_NAME
 
-SCRUB_TARGETS=${KEEP_TARGETS:-1}
+SCRUB_TARGETS=${SCRUB_TARGETS:-0}
 
 LOGGER_ARGS="--trainer.logger.save_dir $SAVE_DIR --trainer.logger.name $EXP_NAME"
 
 
-function v_target_train { echo MinesTargetClassifier_seed${1}; }
-
-function v_target_predict { echo $(v_target_train $1)_predict; }
-
-function v_stance_train { echo MinesStanceClassifier_seed${1}; }
+function v_train { echo MinesTcOneShot_seed${1}; }
 
 
 if [ $FIT -eq 1 ]
@@ -36,35 +31,33 @@ then
     for seed in $SEEDS
     do
         python -m mtse fit \
-            -c configs/base/li_target_classifier.yaml \
+            -c configs/base/mines_tc_oneshot.yaml \
             $LOGGER_ARGS \
-            --trainer.logger.version $(v_target_train $seed) \
+            --trainer.logger.version $(v_train $seed) \
             --seed_everything $seed \
             $EXTRA_ARGS
     done
 else
-    echo "Skipping target fitting"
+    echo "Skipping fitting"
 fi
 
 if [ $TARGET_TEST -eq 1 ]
 then
     EXTRA_ARGS=""
-    if [ $WITH_SE_BUG -eq 1 ]
+    if [ $SCRUB_TARGETS -eq 1 ]
     then
-        EXTRA_ARGS="$EXTRA_ARGS --data.transforms.remove_se_hashtag false"
-    fi
-    if [ $KEEP_TARGETS -eq 1 ]
-    then
-        EXTRA_ARGS="$EXTRA_ARGS --data.transforms.scrub_targets false"
+        EXTRA_ARGS="$EXTRA_ARGS --data.transforms.scrub_targets true"
     fi
 
     for seed in $SEEDS
     do
         python -m mtse test \
-            -c $LOGS_ROOT/$(v_target_train $seed)/config.yaml \
+            -c $LOGS_ROOT/$(v_train $seed)/config.yaml \
             --data configs/data/li_tc_test.yaml \
-            --trainer.logger.version $(v_target_train $seed)_test \
-            --ckpt_path $LOGS_ROOT/$(v_target_train $seed)/checkpoints/*ckpt \
+            --trainer.logger.version $(v_train $seed)_target_test \
+            --trainer.callbacks mtse.callbacks.TargetClassificationStatsCallback \
+            --trainer.callbacks.n_classes 19 \
+            --ckpt_path $LOGS_ROOT/$(v_train $seed)/checkpoints/*ckpt \
             $EXTRA_ARGS
     done
 else
@@ -73,15 +66,22 @@ fi
 
 if [ $STANCE_TEST -eq 1 ]
 then
+    EXTRA_ARGS=""
+    if [ $SCRUB_TARGETS -eq 1 ]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS --data.transforms.scrub_targets true"
+    fi
+
     for seed in $SEEDS
     do
         # We override the existing callback because we're not testing TSE this time
         python -m mtse test \
-            -c $LOGS_ROOT/$(v_stance_train $seed)/config.yaml \
+            -c $LOGS_ROOT/$(v_train $seed)/config.yaml \
             --data configs/data/li_stance_test.yaml \
             --trainer.callbacks mtse.callbacks.StanceClassificationStatsCallback \
-            --trainer.logger.version $(v_stance_train $seed)_test \
-            --ckpt_path $LOGS_ROOT/$(v_stance_train $seed)/checkpoints/*ckpt
+            --trainer.logger.version $(v_train $seed)_stance_test \
+            --ckpt_path $LOGS_ROOT/$(v_train $seed)/checkpoints/*ckpt \
+            $EXTRA_ARGS
     done
 else
     echo "Skipping stance testing"
@@ -89,37 +89,23 @@ fi
 
 if [ $TSE_TEST -eq 1 ]
 then
+    EXTRA_ARGS=""
+    if [ $SCRUB_TARGETS -eq 1 ]
+    then
+        EXTRA_ARGS="$EXTRA_ARGS --data.transforms.scrub_targets true"
+    fi
+
+    # Scrub targets here
     for seed in $SEEDS
     do
-        train_dir=$LOGS_ROOT/$(v_stance_train $seed)
+        train_dir=$LOGS_ROOT/$(v_train $seed)
         python -m mtse test \
             -c $train_dir/config.yaml \
             --ckpt_path $train_dir/checkpoints/*ckpt \
             --data configs/data/li_tse_test.yaml \
-            --data.corpora.target_preds_path $LOGS_ROOT/$(v_target_predict $seed)/target_preds.1.txt \
-            --trainer.callbacks mtse.callbacks.TSEStatsCallback \
-            --trainer.callbacks.full_metrics true \
-            --trainer.logger.version LiTse_seed${seed}
+            --trainer.logger.version $(v_train)_tse_test \
+            $EXTRA_ARGS
     done
 else
     echo "Skipping tse testing"
-fi
-
-if [ $GT_TSE_TEST -eq 1 ]
-then
-    for seed in $SEEDS
-    do
-        train_dir=$LOGS_ROOT/$(v_stance_train $seed)
-        python -m mtse test \
-            -c $train_dir/config.yaml \
-            --ckpt_path $train_dir/checkpoints/*ckpt \
-            --data configs/data/li_tse_test.yaml \
-            --trainer.callbacks mtse.callbacks.TSEStatsCallback \
-            --trainer.callbacks.full_metrics true \
-            --trainer.logger.version LiGtTse_seed${seed} \
-            --data.corpora.target_input label \
-            --model.use_target_gt true
-    done
-else
-    echo "Skipping gt tse testing"
 fi
