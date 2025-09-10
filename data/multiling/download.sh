611a16c437d8211e2d23519396884646ad60fa3f
Ethan L. Mines <elmines@users.noreply.github.com>
2025-09-10 18:01:23 -0400

    Preprocess demonetisation data

--- data/multiling/download.sh
+++ data/multiling/download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+wget https://raw.githubusercontent.com/sahilswami96/StanceDetection_CodeMixed/refs/heads/master/Dataset/Notebandi_tweets.txt
+wget https://raw.githubusercontent.com/sahilswami96/StanceDetection_CodeMixed/refs/heads/master/Dataset/Notebandi_tweets_stance.txt
+
 wget -O raw_et.csv https://raw.githubusercontent.com/markmets/immigration-prediction-EST/refs/heads/main/Annotated_Dataset.csv
 # Lai et al. Data
 wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/lepen_fr.csv
--- utils/multiling_preproc.sh
+++ utils/multiling_preproc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+utils/preproc_hi_data.py data/multiling/
 utils/preproc_et_data.py data/multiling/
-exit 0
 utils/preproc_lai_data.py data/multiling/
