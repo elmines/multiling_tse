c658f536279ef13810fc7cd7dcbf70a6b23accef
Ethan L. Mines <elmines@users.noreply.github.com>
2025-09-10 16:16:45 -0400

    Update data dir

---
+++ data/multiling/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Lai et al. Data
+wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/lepen_fr.csv
+wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/macron_fr.csv
+wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/referendum_it.csv
