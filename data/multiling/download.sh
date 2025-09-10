1da9836d61deddaaccff411c4d66807e86b4f21b
Ethan L. Mines <elmines@users.noreply.github.com>
2025-09-10 17:29:25 -0400

    Preprocess the estonian data

--- data/multiling/download.sh
+++ data/multiling/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wget -O raw_et.csv https://raw.githubusercontent.com/markmets/immigration-prediction-EST/refs/heads/main/Annotated_Dataset.csv
 # Lai et al. Data
 wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/lepen_fr.csv
 wget https://raw.githubusercontent.com/mirkolai/MultilingualStanceDetection/refs/heads/master/dataset/macron_fr.csv
---
+++ utils/multiling_preproc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+utils/preproc_et_data.py data/multiling/
+exit 0
+utils/preproc_lai_data.py data/multiling/
