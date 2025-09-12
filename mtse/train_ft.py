42200c109da9969f850358437f07b0bb8d26b763
Ethan L. Mines <elmines@users.noreply.github.com>
2025-09-12 13:35:09 -0400

    Update train_ft_script

--- mtse/train_ft.py
+++ mtse/train_ft.py
@@ -5,28 +5,41 @@ Train FastText embeddings
 import sys
 import argparse
 import pathlib
+import logging
 # 3rd Party
 from gensim.models import FastText
 from tokenizers.pre_tokenizers import BertPreTokenizer
+from tqdm import tqdm
 # Local
 from .data.parse import *
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--corpus_type', type=str, choices=list(CORPUS_PARSERS.keys()), required=True)
+    parser.add_argument('--corpus_type', nargs="+", type=str, choices=list(CORPUS_PARSERS.keys()), required=True)
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--embed', type=int, default=128)
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('-i', type=pathlib.Path, required=True)
+    parser.add_argument('-i', nargs="+", type=pathlib.Path, required=True)
     parser.add_argument('-o', type=pathlib.Path, required=True)
     args = parser.parse_args(raw_args)
 
-    parse_func = CORPUS_PARSERS[args.corpus_type]
+    logging.basicConfig(level=logging.INFO)
+
+    assert len(args.corpus_type) == len(args.i)
     pretok = BertPreTokenizer()
-    contexts = [ list(map(lambda x: x[0].lower(), pretok.pre_tokenize_str(s.context))) for s in parse_func(args.i)]
+
+    contexts = []
+    for corpus_type, in_path in zip(args.corpus_type, args.i):
+        parse_func = CORPUS_PARSERS[corpus_type]
+        sample_iter = tqdm(parse_func(in_path), desc=f"Parsing {in_path}")
+        contexts.extend(list(map(lambda x: x[0].lower(), pretok.pre_tokenize_str(s.context))) for s in sample_iter)
     model = FastText(vector_size=args.embed, seed=args.seed)
     model.build_vocab(corpus_iterable=contexts)
-    model.train(corpus_iterable=contexts, total_examples=len(contexts), epochs=args.epochs)
+    model.train(
+        corpus_iterable=contexts,
+        total_examples=len(contexts),
+        epochs=args.epochs,
+    )
     out_path = str(args.o)
     model.save(out_path)
     print(f"Saved FastText embeddings to {out_path}")
