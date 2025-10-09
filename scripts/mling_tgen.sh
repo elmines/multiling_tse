0c145515a41724110959bdb188c696562476e327
Ethan L. Mines <elmines@users.noreply.github.com>
2025-10-09 11:30:26 -0400

    Fix if statement

--- scripts/mling_tgen.sh
+++ scripts/mling_tgen.sh
@@ -127,7 +127,7 @@ fi
 if [ $TARGET_TRANS -eq 1 ]
 then
         out_dir=$LOGS_ROOT/fold${fold}_seed${seed}_target_translate
-        if [ -e $out_dir -a ! -z $(ls $out_dir) ]
+        if [ -e $out_dir ] && [ ! -z $(ls $out_dir) ]
         then
             echo Not overwriting existing $out_dir
             exit 1
